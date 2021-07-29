18377cd28c9c43d4360d4b0142212e043b085207
Borja Garcia <64423569+borgarpa@users.noreply.github.com>
2021-07-29 11:19:54 +0200

    Update dsen2cr_main.py

--- Code/dsen2cr_main.py
+++ Code/dsen2cr_main.py
@@ -55,8 +55,8 @@ def run_dsen2cr(predict_file=None, resume_file=None):
 
     dataset_list_filepath = '../Data/datasetfilelist.csv'
 
-    base_out_path = '/path/to/output/model_runs/'
-    input_data_folder = '/path/to/dataset/parent/folder'
+    base_out_path = '/content/dsen2-cr'
+    input_data_folder = '/content/dsen2-cr/data'
 
     # training parameters
     initial_epoch = 0  # start at epoch number
